8835715d3b8b5cc475aa1646f3d079b74770f283
Higor <92187145+HigorCris21@users.noreply.github.com>
2023-05-11 17:46:39 -0300

    Subindo Projeto

---
+++ theCreation.swiftpm/MyApp.swift
@@ -0,0 +1,18 @@
+
+//  the creation
+//
+//  Created by Higor  Lo Castro on 10/04/23.
+//
+
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+           
+        }
+    }
+}
